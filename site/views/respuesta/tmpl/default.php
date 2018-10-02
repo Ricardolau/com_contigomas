994ba9b6780bf4ba0595d876e5d8b43401f06676
Ricardo Oficina <ricardolau6@gmail.com>
2018-10-02 10:14:40 +0200

    Pongo variables globales de respuesta.

--- site/views/respuesta/tmpl/default.php
+++ site/views/respuesta/tmpl/default.php
@@ -8,18 +8,15 @@ JHtml::_('behavior.tooltip');
     <?php echo JText::_('COM_CODIGOMAS_RESPUESTA_TITULO');?>
   </h1>
   <p>
-    <?php echo JText::_('COM_CONTIGOMAS_RESPUESTA_TEXTO_SECUNDARIO');?>
+    <?php echo JText::_('COM_CONTIGOMAS_RESPUESTA_TITULO_SECUNDARIO');?>
   </p>
 <?php
 // Datos del formulario cubierto por si queremos meterlo.. 
 $resultado = $this->resultado;
 ?>
-<ol>
-    <li>Recibirás un e-mail en las próximas horas con tu número de promoción personal</li>
-    <li>Guárdalo y comunícaselo a tu referidos</li>
-    <li>Tus referidos nos comunicarán el número de promoción y cerrarán la venta</li>
-    <li>Ambos recibiréis el obsequio en los siguientes días</li>
-</ol>
+<div>
+    <?php echo JText::_('COM_CONTIGOMAS_RESPUESTA_TEXTO_SECUNDARIO');?>
+</div>
 
 
 </div>
